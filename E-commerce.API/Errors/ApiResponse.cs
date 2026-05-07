1e0e7387b9fe81e468945cc15a535111c273c24a
Mohammed-Maher7 <mohammed.maher.1110@gmail.com>
2026-05-07 19:03:02 +0300

    feat: add ApiResponse class to standardize frontend error responses

---
+++ E-commerce.API/Errors/ApiResponse.cs
@@ -0,0 +1,27 @@
+﻿
+namespace E_commerce.API.Errors
+{
+    public class ApiResponse
+    {
+        public int StatusCode {  get; set; }
+        public string? Message { get; set; }
+
+        public ApiResponse(int statusCode, string? message) 
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultErrorMessageForStatusCode(statusCode);
+        }
+
+        private string? GetDefaultErrorMessageForStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "bad request",
+                401 => "unauthorized",
+                404 => "not found",
+                500 => "server error",
+                _ => null
+            };
+        }
+    }
+}
